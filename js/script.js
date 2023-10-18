77551de938e7bbc6347f04ce45bd10eb38ca01c3
cesarecicolin <cece.caster@gmail.com>
2023-10-18 16:45:47 +0200

    add: logia del programa

--- js/script.js
+++ js/script.js
@@ -5,6 +5,43 @@ const age = parseInt(prompt("quanti anni hai ?"))
 console.log(age, typeof age)
 const costOfTravel = (0.21 * km)
 console.log(costOfTravel)
+// logica
+
+let discount =""
+
+if (age <18)
+{
+   discount = (costOfTravel * 0.20 % 100)
+   console.log(discount)
+}
+else if (age >65)
+{
+   discount = (costOfTravel * 0.40 % 100)
+}
+else 
+{
+   discount = (costOfTravel)
+}
+
+let totalPrice =""
+if (age <18 || age >65)
+{
+   totalPrice = (costOfTravel - discount)
+}
+else {
+   totalPrice = (costOfTravel)
+}
+
+
+
+const ticket = totalPrice. toFixed(2)
+console.log(ticket, typeof ticket)
+
+const message = `il prezzo del tuo biglieto è ${ticket}€`
+
+// OUTPUT
+
+document.getElementById("ticket") .innerHTML = message
 
 
 
